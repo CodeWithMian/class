e6c808f18727b3ebd39c98d6d66abc3c0a875fa7
Bilal <mb03086350364@gmail.com>
2024-01-21 20:39:07 +0500

    Node Js

--- api/user.js
+++ api/user.js
@@ -3,8 +3,21 @@ const userModel = require("../model/user");
 const bcrypt = require("bcrypt");
 router.post("/register", async (req, res) => {
   try {
+    if (!req?.body?.password) {
+      res.status(400).json({
+        success: false,
+        message: "Enter Password",
+      });
+    }
     const hashpass = await bcrypt.hash(req.body.password, 10);
+    const newusername = req.body.userName?.split(" ");
 
+    if (newusername?.length >= 2) {
+      res.status(400).json({
+        success: false,
+        message: "Spaces Are Not Allowed",
+      });
+    }
     const user = await userModel.create({ ...req.body, password: hashpass });
 
     res.status(200).json({
@@ -23,6 +36,12 @@ router.post("/register", async (req, res) => {
         res.status(409).json({
           success: false,
           message: "Phone Already in Use!",
+        });
+      }
+      if (error.keyPattern?.userName) {
+        res.status(409).json({
+          success: false,
+          message: "UserName Already in Use!",
         });
       }
       return;
@@ -44,4 +63,142 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+    if (!userName) {
+      res.status(400).json({
+        success: false,
+        message: "Enter userName",
+      });
+    }
+    if (!password) {
+      res.status(400).json({
+        success: false,
+        message: "Enter Password",
+      });
+    }
+    const founduser = await userModel.findOne({ userName: userName });
+    if (!founduser) {
+      res.status(400).json({
+        success: false,
+        message: "Invalid Credentiols",
+      });
+    } else {
+      const validatepass = await bcrypt.compare(password, founduser.password);
+
+      if (!validatepass) {
+        res.status(400).json({
+          success: false,
+          message: "Invalid Credentiols",
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: "Login Successfully",
+        });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    if (error.message.split(",")[0]?.split(":")[2]?.trim()) {
+      res.status(400).json({
+        success: false,
+        message: error.message.split(",")[0]?.split(":")[2]?.trim(),
+      });
+      return;
+    }
+  }
+});
+router.get("/get", async (req, res) => {
+  try {
+    var match = {};
+    if (req.query.userName) {
+      match.userName = new RegExp(req.query.userName, "i");
+    }
+
+    const allusers = await userModel.find(match, { userName: 1 });
+    res.status(200).json({
+      success: true,
+      message: allusers,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/:_id", async (req, res) => {
+  try {
+    const user = await userModel.findById(req.params._id);
+
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+      });
+    }
+    res.status(201).json({
+      success: true,
+      message: user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      success: false,
+      message: "User Not Found",
+    });
+  }
+});
+router.put("/:_id", async (req, res) => {
+  try {
+    const user = await userModel.findById(req.params._id);
+
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+      });
+    }
+    const hashpass = await bcrypt.hash(req.body.password, 10);
+
+    const updateduser = await userModel.findByIdAndUpdate(user._id, {
+      $set: { ...req.body, password: hashpass },
+      
+    }, {new: true});
+
+    res.status(201).json({
+      success: true,
+      message: updateduser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      success: false,
+      message: "User Not Found",
+    });
+  }
+});
+router.delete("/:_id", async (req, res) => {
+  try {
+    const user = await userModel.findById(req.params._id);
+
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+      });
+    }
+
+    const deleteduser = await userModel.findByIdAndDelete(req.params._id)
+    res.status(201).json({
+      success: true,
+      message: "User Deleted ",
+    });
+  } catch (error) {
+    
+    res.status(404).json({
+      success: false,
+      message: "User Not Found",
+    });
+  }
+});
 module.exports = router;
--- model/user.js
+++ model/user.js
@@ -1,36 +1,38 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,"Name is Required"]
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is Required"],
     },
-    phone:{
-        type:Number,
-        required:[true,"Phone is Required"],
-        unique:true
+    phone: {
+      type: Number,
+      required: [true, "Phone is Required"],
+      unique: true,
     },
-    email:{
-        type:String,
-        required:[true,"Email is Required"],
-        unique:true
+    email: {
+      type: String,
+      required: [true, "Email is Required"],
+      unique: true,
     },
-    userName:{
-        type:String,
-        required:[true,"UserName is Required"],
-        unique:true
+    userName: {
+      type: String,
+      required: [true, "UserName is Required"],
+      unique: true,
     },
-    photo:{
-        type:String,
-        required:[true,"Photo is Required"]
+    photo: {
+      type: String,
+      required: [true, "Photo is Required"],
     },
-    password:{
-        type:String,
-        required:[true,"Password is Required"]
-    }
-},
-{
-    timeStamps:true
-})
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+    },
+  },
+  {
+    timeStamps: true,
+  }
+);
 
-module.exports = mongoose.model("users", userSchema)
+module.exports = mongoose.model("users", userSchema);
